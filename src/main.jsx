9782088c5169606fe363b423d9dda0397b5a9569
Fardeen72 <im.feedi4u@gmail.com>
2026-02-23 10:58:35 +0530

    build passes, ready for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,6 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
-
-
 import "./index.css";
 import Layout from "./Layout.jsx";
 
@@ -17,7 +15,7 @@ import Home from "./components/Home/Home.jsx";
 import About from "./components/About/About.jsx";
 import Contact from "./components/Contact/Contact.jsx";
 
-// Questions (QUIZZES)
+// Quiz pages
 import Html from "./components/Questions/Html.jsx";
 import Css from "./components/Questions/Css.jsx";
 import Js from "./components/Questions/Js.jsx";
@@ -29,7 +27,7 @@ import TailwindQuiz from "./components/Questions/tailwind.jsx";
 import MongodbQuiz from "./components/Questions/mongo.jsx";
 import RestApiQuiz from "./components/Questions/RestApi.jsx";
 
-// Learning pages
+// Learn pages
 import HtmlLearn from "./components/Questions/HtmlLearn.jsx";
 import CssLearn from "./components/Questions/CssLearn.jsx";
 import JsLearn from "./components/Questions/Jslearn.jsx";
@@ -43,20 +41,16 @@ import DSALearn from "./components/Questions/dsalearn.jsx";
 import NodeLearn from "./components/Questions/Nodelean.jsx";
 import RESTLearn from "./components/Questions/Restlrarn.jsx";
 
-// Cards & Tutorials
+// Cards & content
 import QuizCard from "./components/Cards/Card.jsx";
 import TutorialsCard from "./components/Tutorials/Tutorials.jsx";
-import CoursesCard from "./components/Tutorials/Tutorials.jsx";
 
 // Other pages
 import Leaderboard from "./leaderbord.jsx";
 import BlogPage from "./blog.jsx";
 import ChallengePage from "./components/Home/Addon.jsx";
 import CheatsheetsPage from "./components/cheatsheet.jsx";
-import Components from "./components/Compountes.jsx";
-
-// Auth
-import LoginButton from "./components/Contact/LoginWithgoogle.jsx";
+import Compountes from "./components/Compountes.jsx";
 import NavbarProfile from "./components/Contact/LoginWithgoogle.jsx";
 
 const router = createBrowserRouter(
@@ -65,47 +59,48 @@ const router = createBrowserRouter(
       <Route index element={<Home />} />
       <Route path="about" element={<About />} />
       <Route path="contact" element={<Contact />} />
-      <Route path="loginbutton" element={<NavbarProfile />} />
+      <Route path="profile" element={<NavbarProfile />} />
       <Route path="challenge" element={<ChallengePage />} />
+
+      {/* Learn */}
       <Route path="htmllearn" element={<HtmlLearn />} />
-      <Route path="blog" element={<BlogPage />} />
-      <Route path="leaderboard" element={<Leaderboard />} />
-  
-      <Route path="tutorials" element={<TutorialsCard />} />
       <Route path="csslearn" element={<CssLearn />} />
       <Route path="jslearn" element={<JsLearn />} />
       <Route path="reactlearn" element={<ReactLearn />} />
-      <Route path="quizzses" element={<HtmlLearn />} />
-       <Route path="sqllearn" element={<SqlLearn />} />
-      <Route path="html" element={<Html />} />
-      <Route path="react" element={<ReactQuesion />} />
-      <Route path="css" element={<Css />} />
-      <Route path="js" element={<JS />} />
-      <Route path="tailwindlearn" element={<TailwindLearn />} />
-      <Route path="java" element={<JavaQuestion />} />
-      <Route path="javalearn" element={<JavaLearn />} />
-      <Route path="python" element={<PythonQuestion />} />
       <Route path="pythonlearn" element={<PythonLearn />} />
+      <Route path="javalearn" element={<JavaLearn />} />
+      <Route path="tailwindlearn" element={<TailwindLearn />} />
       <Route path="gitgithublearn" element={<GitGithubLearn />} />
       <Route path="firebaselearn" element={<FirebaseLearn />} />
       <Route path="dsalearn" element={<DSALearn />} />
       <Route path="nodelearn" element={<NodeLearn />} />
       <Route path="restapilearn" element={<RESTLearn />} />
+
+      {/* Quiz */}
+      <Route path="html" element={<Html />} />
+      <Route path="css" element={<Css />} />
+      <Route path="js" element={<Js />} />
+      <Route path="react" element={<ReactQuestion />} />
+      <Route path="python" element={<PythonQuestion />} />
+      <Route path="java" element={<JavaQuestion />} />
       <Route path="nodequiz" element={<NodeQuiz />} />
       <Route path="tailwind" element={<TailwindQuiz />} />
       <Route path="mongodbquiz" element={<MongodbQuiz />} />
-        <Route path="typescriptlearn" element={<TypeScriptLearn/>} />
-       <Route path="mongodblearn" element={<MongoDBLearn/>} />
-         <Route path="cheatsheet" element={<CheatsheetsPage/>} />
-        <Route path="restquiz" element={<RestApiQuiz/>} />
-         <Route path="components" element={<Compountes/>} />
-           <Route path="cards" element={<QuizzCard/>} />
-    </Route>,
-  ),
+      <Route path="restquiz" element={<RestApiQuiz />} />
+
+      {/* Other */}
+      <Route path="tutorials" element={<TutorialsCard />} />
+      <Route path="cards" element={<QuizCard />} />
+      <Route path="cheatsheet" element={<CheatsheetsPage />} />
+      <Route path="components" element={<Compountes />} />
+      <Route path="leaderboard" element={<Leaderboard />} />
+      <Route path="blog" element={<BlogPage />} />
+    </Route>
+  )
 );
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
+  </StrictMode>
 );
